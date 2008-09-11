54af434b1e9f3766a4e9aaf970c60b49be34a41f
Ben Johnson <bjohnson@binarylogic.com>
2008-09-11 12:39:58 -0400

    Change eval statement

--- lib/boolean_datetime_attribute.rb
+++ lib/boolean_datetime_attribute.rb
@@ -8,26 +8,25 @@ module BinaryLogic
       def boolean_datetime_attribute(*datetime_fields)
         datetime_fields.each do |field|
           boolean_field = field.to_s.gsub(/_at$/, "")
-          src = "def #{boolean_field}=(value)
-            if (value == true || value == 'true' || value == 1 || value == '1')
-              self.#{field} = Time.now if !#{boolean_field}?
-            else
-              self.#{field} = nil if #{boolean_field}?
+          class_eval <<-"end_eval", __FILE__, __LINE__
+            def #{boolean_field}=(value)
+              if (value == true || value == 'true' || value == 1 || value == '1')
+                self.#{field} = Time.now if !#{boolean_field}?
+              else
+                self.#{field} = nil if #{boolean_field}?
+              end
+              #{field}
             end
-            #{field}
-          end
         
-          def #{boolean_field}?
-            !#{field}.blank? && #{field} <= Time.current
-          end
-          alias_method :#{boolean_field}, :#{boolean_field}?
+            def #{boolean_field}?
+              !#{field}.blank? && #{field} <= Time.current
+            end
+            alias_method :#{boolean_field}, :#{boolean_field}?
           
-          def #{boolean_field}!
-            update_attribute :#{field}, Time.now
-          end
-          "
-        
-          class_eval src
+            def #{boolean_field}!
+              update_attribute :#{field}, Time.now
+            end
+          end_eval
         end
       end
     end
